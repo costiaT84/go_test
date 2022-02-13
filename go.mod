0269d850a9e558dc3b4fc3e0244723ffe75da0ac
costiaT84 <costiata@gmail.com>
2022-02-13 18:00:35 +0200

    some init msg

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/costiaT84/go_test
+
+go 1.17
---
+++ mypackage/actions/arrays.go
@@ -0,0 +1,10 @@
+package actions
+// Checks if a number is prime or not
+func IsNotPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i != 0 {
+			return false
+		}
+	}
+	return true
+}
