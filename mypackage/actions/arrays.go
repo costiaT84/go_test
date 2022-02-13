b5f451dac00f067321792a8aaa093c025b1ca549
costiaT84 <costiata@gmail.com>
2022-02-13 18:27:17 +0200

    changed did for test

--- mypackage/actions/arrays.go
+++ mypackage/actions/arrays.go
@@ -1,6 +1,16 @@
 package actions
+
 // Checks if a number is prime or not
 func IsNotPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func test(num int) bool {
 	for i := 2; i < int(num/2); i++ {
 		if num%i != 0 {
 			return false
